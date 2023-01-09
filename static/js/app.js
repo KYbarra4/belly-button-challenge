f54dbdf813161731601804ad9d177b9c805395f7
Kevin Ybarra <kevin.ybarra@gmail.com>
2023-01-08 22:18:26 -0600

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,7 @@
 // Use the D3 library to read in samples.json from url
 // let url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
-// // Use sample_values as the values for the bar chart.
-// let sample_values = selectID.sample_values
-
-// // Use otu_ids as the labels for the bar chart.
-// let otu_ids = selectID.otu_ids
-
-// // Use otu_labels as the hovertext for the chart.
-// let otu_labels = selectID.otu_labels
-
 function init() {
-    // let dropDown = d3.select('#selDataset');
 
-// Promise Pending
-    // let bellyPromise = d3.json('samples.json');
     d3.json('samples.json').then(function (data) {
         console.log(data)
 
@@ -22,31 +10,6 @@ function init() {
         names.forEach((name) => {
             dropdownMenu.append('option').text(name).property('value', name);        
         });
-
-       
-
-    //     console.log('Promise :', bellyPromise);
-
-// Fetch the JSON data and console log it
-// d3.json(bellyPromise).then(function(data) {
-//     console.log(data);
-// });
-
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-// let names = data.names;
-
-// let dropDown = d3.select('#selDataset');
-    // function init() {
-    //     let dropDown = d3.select('#selDataset');
-
-    //     let names = data.names;
-
-    //     names.forEach((id) => {
-            // console.log(id);
-    //         select.append('option')
-    //         .text(id)
-    //         .property('value', id);        
-    //     });
 
         let beginSample = names[0];
 
@@ -80,20 +43,6 @@ function buildDemo(samples) {
     });
 }
 
-// function buildChart(sample)
-// function optionChanged(dropSample) {
-//     buildChart(dropSample);
-//     buildMetadata(dropSample);
-// }
-// // Use sample_values as the values for the bar chart.
-//     function buildMetadata(sample) {
-//         d3.json(url).then(function(data) {
-//             let meta = data.metadata;
-//             let domoInfo = metadata.filter(sample_values)
-//     });
-// }
-//     });
-// }
 function buildChart(samples) {
     d3.json('samples.json').then((data) => {
         let sampleData = data.samples;
@@ -109,28 +58,20 @@ function buildChart(samples) {
         console.log(metaFirst)
 
 // Use sample_values as the values for the bar chart.
-        let sample_values = selectID.sample_values
+        let sample_values = selectID.sample_values;
 
 // Use otu_ids as the labels for the bar chart.
-        let otu_ids = selectID.otu_ids
+        let otu_ids = selectID.otu_ids;
 
 // Use otu_labels as the hovertext for the chart.
-        let otu_labels = selectID.otu_labels
-
-        // var wfreq = parseFloat(information.wfreq)
-        // console.log(wfreq)
-
-        // let barSort = [otu_ids];
-        // barSort.sort(function compareFunction(firstNum, secondNum) {
-        //     return secondNum - firstNum;
-        // });
+        let otu_labels = selectID.otu_labels;
 
         let trace1 = [{
 
             x: sample_values.slice(0, 10).reverse(),
             y: otu_ids.slice(0, 10).map((OTU) => 'OTU ${OTU}').reverse(),
 
-            name: 'Top 10 OTUs',
+            // name: 'Top 10 OTUs',
             labels: otu_labels.slice(0,10).reverse(),
             type: 'bar',
             orientation:'h'
@@ -142,6 +83,7 @@ function buildChart(samples) {
         let trace2 = [{
             x: otu_ids,
             y: sample_values,
+            // title: 'OTU Bubble Data',
             labels: otu_labels,
             mode: 'markers',
             marker: {
@@ -154,9 +96,7 @@ function buildChart(samples) {
 
         Plotly.newPlot('bubble', trace2);
 
-    // });
-// });
     });
 }
 
-// init();
+
